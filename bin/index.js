66fa220054c5bae6dd2899eb84aaac0bcb8bc4a8
Tom Gordon <tom.gordon@squareweave.com.au>
2021-01-07 11:00:04 +1100

    Initial files

---
+++ bin/index.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const fs = require("fs");
+
+const argv = require("minimist")(process.argv);
+const colors = require("colors/safe");
+const { minifyImages, generateSprite } = require('../lib/generators');
+
+const SRC = argv.src || null;
+const SPRITE = argv.sprite || null;
+const PREPENDID = argv["prepend-symbol-id"] || "svg-";
+const CSSCLASS = argv["css-class"] || "svg-sprite";
+
+const generate = async () => {
+    if (!SRC) {
+        console.log(
+            colors.red.underline(
+                "Please specify a glob --src path for the folder of SVGs to create the sprite from eg. example/**/*.svg"
+            )
+        );
+        process.exit(1);
+    }
+    if (!SPRITE) {
+        console.log(
+            colors.red.underline(
+                "Please specify a --sprite svg path and file to generate to. eg. example/dist/svg-sprite.svg, it works with any extension: eg. $npm_package_config_theme/templates/partials/svg/symbol/svg-sprite.twig"
+            )
+        );
+        process.exit(1);
+    }
+    try {
+        const transformedFiles = await minifyImages(SRC, SPRITE);
+        await generateSprite(transformedFiles, CSSCLASS, SPRITE,PREPENDID);
+        console.log(colors.green(`SVGSprite svg file generated: ${SPRITE}`));
+    } catch (e) {
+		console.log(colors.red(`Something went wrong: ${e} `));
+		process.exit(1);
+    }
+};
+
+module.exports = generate();
---
+++ readme.md
@@ -0,0 +1,28 @@
+# SVG generator
+
+`npm install svgsprite-file --save`
+
+Very basic package to create an svg file from a folder of svgs. Intended to be used as part of an NPM scripts definition ie.
+
+## Example
+
+```
+{
+    "scripts": {
+        "generate-sprite": "svgsprite-file --src src/svg-icons/**/*.svg --sprite dist/svg-icons/svg-sprite.svg"
+    }
+}
+```
+
+## Config
+
+`--src` = Glob file path for svgs to include in sprite generation.
+eg. `--src $npm_package_config_src/src/svg-icons/**/*.svg`
+
+`--sprite` = File path and name SVGs/svg sprite file to be generated into.
+eg. `$npm_package_config_src/dist/svg-icons/svg-sprite.svg`
+eg. `$npm_package_config_theme/templates/partials/svg/symbol/svg-sprite.twig`
+
+`--prepend-symbol-id` = Optional; default: `svg-`
+
+`--css-class` = Optional; default: `svg-sprite`
